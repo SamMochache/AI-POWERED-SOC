c5beb185368e68a92c2491ee693609ef19baee40
SamMochache <sammochache01@gmail.com>
2025-03-07 02:33:30 +0300

    Adding models in the admin database panel postman is working

--- soc_backend/monitoring/admin.py
+++ soc_backend/monitoring/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Log, Threat, Alert
 
-# Register your models here.
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp', 'source_ip', 'destination_ip', 'protocol', 'status')
+    search_fields = ('source_ip', 'destination_ip', 'protocol')
+    list_filter = ('status', 'protocol', 'timestamp')
+
+@admin.register(Threat)
+class ThreatAdmin(admin.ModelAdmin):
+    list_display = ('log', 'threat_level', 'description')
+    list_filter = ('threat_level',)
+    search_fields = ('description',)
+
+@admin.register(Alert)
+class AlertAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('message',)
